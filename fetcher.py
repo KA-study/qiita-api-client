19bdba8a0b929bb7de2806fe9065ff00075621bf
KeigoAdachi <k.adachi.contact@gamil.com>
2026-04-28 18:20:09 +0900

    implement fetcher to interact with the API

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,20 @@
 # fetch qiita articles
+import requests
+from config import URL
 
 
+def fetcher(params: dict) -> list | dict:
+
+    try:
+        res = requests.get(URL, params=params, timeout=10)
+        res.raise_for_status()
+
+        print(res.url)
+
+        return res.json()
+    except requests.exceptions.Timeout:
+        raise RuntimeError("Request timeout")
+    except requests.exceptions.HTTPError as ex:
+        raise RuntimeError(f"HTTP error: {ex}")
+    except ValueError:
+        raise RuntimeError("Invalid JSON response")
