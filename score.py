f17d5521417150ee1fefb6141325a9d41949d15f
KeigoAdachi <k.adachi.contact@gamil.com>
2026-05-06 01:18:54 +0900

    Fixed a bug in carc_tag_score and made improvements.

--- score.py
+++ score.py
@@ -4,24 +4,43 @@ from datetime import datetime
 from data_storage.scheme import ActivityData, ActivityMap, ActivityItem
 from config import TAU, SECONDS_PER_DAY
 
-def calc_tag_score(tags_logs: ActivityMap, tag: str) -> float:
-    tag_score = 0
-
+def calc_tag_score(tags_logs: ActivityMap, tags: list[str]) -> float:
     #記事に一つもtagが付されていないとき
-    if tag is None or tag == "":
-        ...
+    #本当に0.0を返してよいのか。後ほど要検討
+    if not tags:
+        return 0.0
 
-    #logsを初期化するときに、（存在するのであれば、つまり上の分岐に引っかからないのであれば以下は成り立つ
+    #tags_logsが空だった（一度もtag指定で検索したことがない）とき
     all_count: int = sum(tags_logs[tag_name]["count"] for tag_name in tags_logs)
-    count: int = tags_logs[tag]["count"]
-    last_used = datetime.fromisoformat( tags_logs[tag]["last_used"] )
+    if not all_count:
+        return 0.0
 
+    score_list = []
     now = datetime.now()
 
-    delta = now - last_used
-    elapsed_day = delta.total_seconds() / SECONDS_PER_DAY
-    
-    return (count/all_count)*math.exp(-elapsed_day/TAU) 
+    for tag in tags:
+
+        #tags_logsにarticleのtagが含まれているとは限らない
+        if tag not in tags_logs:
+            continue
+
+        #json書き込み失敗時、また後程manage_params()などを拡張したときに、last_usedがNoneのまま通ってくる可能性をカット
+        if not tags_logs[tag]["last_used"]:
+            continue
+
+        count: int = tags_logs[tag]["count"] 
+        last_used = datetime.fromisoformat( tags_logs[tag]["last_used"] )
+
+        elapsed_day = (now - last_used).total_seconds() / SECONDS_PER_DAY
+        
+        score_list.append((count/all_count)*math.exp(-elapsed_day/TAU)) 
+
+    #記事にtagが付されていて、かつtags_logsもからでないが、tags_logsにあてはまるtagが一つもなかった時。
+    if not score_list:
+        return 0.0   
+
+    #スコアリングで、最大値以外すべて無視している。後ほど要再設計。
+    return max(score_list) 
 
 def calc_keyword_score() -> int:
 
