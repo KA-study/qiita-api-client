55a04dfdf0e17542bb8a3a940e2d5cfba13bc6fc
KeigoAdachi <k.adachi.contact@gamil.com>
2026-05-11 17:55:30 +0900

    add normalize_for_ai function.

--- ai/manager.py
+++ ai/manager.py
@@ -5,7 +5,8 @@ from ai.processor import normalize_for_ai
 
 
 def ai_manager(data_list: list[ArticleData]) -> list:
+    # 安全のため
     ai_data_list = copy.deepcopy(data_list)
 
     for ai_data in ai_data_list:
-        ai_data["body"] = normalize_body(ai_data["body"])
+        ai_data = normalize_for_ai(ai_data)
--- ai/processor.py
+++ ai/processor.py
@@ -1,4 +1,6 @@
 import re
+from storage.scheme import ArticleData
+from ai.class_manager import AIArticleData
 
 
 def normalize_body(body: str) -> str:
@@ -16,3 +18,19 @@ def normalize_body(body: str) -> str:
     body = re.sub(r"\n{3,}", "\n\n", body)
 
     return body.strip()
+
+
+def normalize_for_ai(data: ArticleData) -> AIArticleData:
+    ai_data: AIArticleData = {
+        "id": "",
+        "title": "",
+        "body": "",
+        "tags": [],
+    }
+
+    ai_data["id"] = data["id"]
+    ai_data["title"] = data["title"]
+    ai_data["body"] = normalize_body(data["body"])
+    ai_data["tags"] = data["tags"]
+
+    return ai_data
