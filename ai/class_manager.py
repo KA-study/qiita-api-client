46ac9c73dc978f91b7ad6eeef1457b37a671dcea
KeigoAdachi <k.adachi.contact@gamil.com>
2026-05-12 09:59:29 +0900

    rename the file: class_manager to definitions, add some class for ai processing.

--- ai/class_manager.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict
-
-
-class AIArticleData(TypedDict):
-    id: str
-    title: str
-    body: str
-    tags: list[str]
---
+++ ai/definitions.py
@@ -0,0 +1,41 @@
+from typing import TypedDict, Literal
+from enum import StrEnum
+
+
+class AIArticleData(TypedDict):
+    data_type: Literal["article"]
+
+    id: str
+    title: str
+    body: str
+    tags: list[str]
+
+
+class AIExecutionData(TypedDict):
+    data_type: Literal["excution"]
+
+    id: str
+    title: str
+    body: str
+    tags: list[str]
+    hash_value: str  # 本文ハッシュ結果
+
+
+# StrEnumにすることで、実質的にdictとして扱える。(Enuオブジェクトじゃなくてstrとしてふるまう)
+class TargetAudienceLevel(StrEnum):
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermidiate"
+    ADVANCED = "advanced"
+
+
+class AIProcessedData(TypedDict):
+    # 記事データ
+    id: str
+    title: str
+    body: str
+    tags: list[str]
+    hash_value: int
+
+    # 処理後データ
+    summary: str
+    audiencelevel: TargetAudienceLevel
