f37a115365756200f5ebd51e3bcd112cfa3ea7a7
KeigoAdachi <k.adachi.contact@gamil.com>
2026-05-12 23:33:17 +0900

    add DB_processor  function and fetch one column function.

--- ai/definitions.py
+++ ai/definitions.py
@@ -1,6 +1,29 @@
 from typing import TypedDict, Literal
 from enum import StrEnum
 
+# このLiteralにふくまれる文字列はCREATE_AI_PROCESSED_TABLEの列項目であるが、スペルミスが極めて起こりやすいため、
+# AIArticleDataをEnum型継承にするなどして、対応すること。
+TABLE_COLUMN_ITEM = Literal[
+    "title", "body", "hash_value", "tags", "summary", "audience_level"
+]
+
+
+CREATE_AI_PROCESSED_TABLE = """
+CREATE TABLE IF NOT EXISTS ai_processed(
+    id TEXT PRIMARY KEY,
+    
+    --記事メタ
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    hash_value TEXT NOT NULL,
+    tags TEXT NOT NULL, --JSON文字列に変換しておく。
+    
+    --AI処理結果
+    summary TEXT NOT NULL,
+    audience_level TEXT NOT NULL
+    )
+"""
+
 
 class AIArticleData(TypedDict):
     data_type: Literal["article"]
@@ -35,7 +58,7 @@ class AIProcessedData(TypedDict):
     title: str
     body: str
     tags: list[str]
-    hash_value: int
+    hash_value: str
 
     # 処理後データ
     summary: str
--- ai/processor.py
+++ ai/processor.py
@@ -1,8 +1,14 @@
 import re
 import hashlib
+import sqlite3
 
 from storage.scheme import ArticleData
-from ai.class_manager import AIArticleData
+from ai.definitions import (
+    AIArticleData,
+    AIExecutionData,
+    CREATE_AI_PROCESSED_TABLE,
+    TABLE_COLUMN_ITEM,
+)
 
 
 def normalize_body(body: str) -> str:
@@ -43,3 +49,34 @@ def normalize_for_ai(data: ArticleData) -> AIArticleData:
     ai_data["hash_value"] = hash_body(data["body"])
 
     return ai_data
+
+
+def fetch_one_column(column_key: TABLE_COLUMN_ITEM, conn: sqlite3.Connection) -> list:
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT {column_key} FROM ai_processed")
+
+    rows = cursor.fetchall()
+
+    # 以下の判別は変更に弱い。方法を変更すること。
+    if not column_key == "title" and column_key in TABLE_COLUMN_ITEM:
+        for row in rows:
+            row = row[0]
+        return rows
+    elif column_key == "tags":
+        return rows
+
+    raise ValueError(
+        f"Invalid value: {column_key} was selected as column_key of ai_processed TABLE."
+    )
+
+
+def DB_processor(ai_data_list: list[AIArticleData]) -> list[AIExecutionData]:
+
+    ai_processed_conn = sqlite3.connect("ai_processed_data.db")
+    ai_processed_conn.execute(CREATE_AI_PROCESSED_TABLE)
+
+    try:
+        hash_values: list = fetch_one_column("hash_value", ai_processed_conn)
+
+    finally:
+        ai_processed_conn.close()
