e6e5c2c89a04ec3cb87793ba2ffbb3ddcd2d28d3
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-14 14:33:36 +0900

    imprement the convert_article_to_execution function.(add a statement to a decision_logs.)

--- ai/processor.py
+++ ai/processor.py
@@ -3,12 +3,8 @@ import hashlib
 import sqlite3
 
 from storage.scheme import ArticleData
-from ai.definitions import (
-    AIArticleData,
-    AIExecutionData,
-    CREATE_AI_PROCESSED_TABLE,
-    TABLE_COLUMN_ITEM,
-)
+from ai.definitions import (AIArticleData, AIExecutionData, CREATE_AI_PROCESSED_TABLE,
+                            TABLE_COLUMN_ITEM,)
 
 
 def normalize_body(body: str) -> str:
@@ -57,7 +53,7 @@ def fetch_one_column(column_key: TABLE_COLUMN_ITEM, conn: sqlite3.Connection) ->
 
     rows = cursor.fetchall()
 
-    # 以下の判別は変更に弱い。方法を変更すること。
+    #以下の判別は変更に弱い。方法を変更すること。
     if not column_key == "title" and column_key in TABLE_COLUMN_ITEM:
         for row in rows:
             row = row[0]
@@ -65,10 +61,29 @@ def fetch_one_column(column_key: TABLE_COLUMN_ITEM, conn: sqlite3.Connection) ->
     elif column_key == "tags":
         return rows
 
-    raise ValueError(
-        f"Invalid value: {column_key} was selected as column_key of ai_processed TABLE."
-    )
+    raise ValueError(f"Invalid value: {column_key} was selected as column_key of ai_processed TABLE.") 
 
+
+#hash_valuesは、今までにAI処理をした記事の本文ハッシュ値リスト
+def convert_article_to_execution(ai_data_list: list[AIArticleData], hash_values: list) -> list[AIExecutionData]:
+
+    ai_execution_list: list[AIExecutionData] = [] 
+
+    #ai_articleからai_executionに移す。
+    for ai_data, ai_execution_data in zip(ai_data_list, ai_execution_list):
+
+        if ai_data["hash_value"] in hash_values:
+            ai_execution_data["reuse"] = True
+        else:
+            ai_execution_data["id"] = ai_data["id"]
+            ai_execution_data["title"] = ai_data["title"]
+            ai_execution_data["body"] = ai_data["body"]
+            ai_execution_data["tags"] = ai_data["tags"]
+            ai_execution_data["hash_value"] = ai_data["hash_value"]
+
+            ai_execution_data["reuse"] = False
+
+    return ai_execution_list
 
 def DB_processor(ai_data_list: list[AIArticleData]) -> list[AIExecutionData]:
 
@@ -78,5 +93,10 @@ def DB_processor(ai_data_list: list[AIArticleData]) -> list[AIExecutionData]:
     try:
         hash_values: list = fetch_one_column("hash_value", ai_processed_conn)
 
+        ai_execution_list = convert_article_to_execution(ai_data_list, hash_values)
+
+
     finally:
         ai_processed_conn.close()
+
+
